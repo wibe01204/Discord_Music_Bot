f3fcc330df30419633a5108efb9e250a4064fe2b
a951087423 <v0973570232@gmail.com>
2021-09-22 22:16:21 +0800

    Create discord music bot.py

---
+++ discord music bot.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+import os
+
+TOKEN = 'TOKEN'
+BOT_PREFIX = '!'
+
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print("Logged in as: " + bot.user.name + "\n")
+
+
+@bot.command(pass_context=True, aliases=['j'])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await voice.disconnect()
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"狗狗衝進了 {channel}")
+
+
+@bot.command(pass_context=True, aliases=['l'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"法鬥向外衝出了 {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("我他媽沒在裡面")
+
+
+@bot.command(pass_context=True, aliases=['p'])
+async def play(ctx, url: str):
+
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("錯誤: 歌還在放啦")
+        return
+
+    await ctx.send("等我一下")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("正在下載歌曲\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
+
+bot.run(TOKEN)
